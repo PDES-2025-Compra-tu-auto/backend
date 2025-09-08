0a86c4823f3c4da1c5dfe1159061aad07cd9d772
TamaraBenitez <tamaraelizabethbenitez98@gmail.com>
2025-09-07 23:54:57 -0300

    authorization

--- src/infraestructure/controllers/concessionary-agency/concessionary-agency.controller.ts
+++ src/infraestructure/controllers/concessionary-agency/concessionary-agency.controller.ts
@@ -3,10 +3,11 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { ConcessionaryAgencyService } from 'src/application/concessionary-agency/services/concessionary-agency.service';
 import { CreateConcessionaryAgencyDto } from './dto/create-concessionary-agency.dto';
 import { AuthGuard } from 'src/infraestructure/guards/auth.guard';
+import { RolesGuard } from 'src/infraestructure/guards/roles.guard';
+import { Roles } from 'src/infraestructure/decorators/roles.decorator';
+import { UserRole } from 'src/domain/user/enums/UserRole';
 
 @ApiTags('ConcessionaryAgency')
-@UseGuards(AuthGuard)
-@ApiBearerAuth()
 @Controller('concessionaryAgency')
 export class ConcessionaryAgencyController {
   constructor(
@@ -20,6 +21,10 @@ export class ConcessionaryAgencyController {
     return this.concessionaryAgencyService.create(createConcessionaryAgencyDto);
   }
 
+  //ejemplo de prueba, no va aplicar exactamente asi.
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(UserRole.DEALER, UserRole.ADMINISTRATOR)
+  @ApiBearerAuth()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.concessionaryAgencyService.findOneById(id);
