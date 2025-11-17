992f0b93c9c677d59d43b96f90ff6afe01106fbe
Diego Moronha <diegomoronha@gmail.com>
2025-11-16 22:07:58 -0300

    add k6 specifications

--- test/load/auth.test.js
+++ test/load/auth.test.js
@@ -1,66 +1,69 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { randomString, randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+import { uuidv4, randomItem } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-  vus: 3,
-  duration: '30s',
+    stages: [
+        { duration: '30s', target: 10 },   
+        { duration: '45s', target: 50 },     
+        { duration: '1m', target: 120 },
+        { duration: '45s', target: 50 },    
+        { duration: '30s', target: 0 },   
+    ],
+};
+const host = { 
+    url: __ENV.HOST_URL || 'http://localhost',
+    port: __ENV.HOST_PORT || '4000',
+    toString: function() {
+        return `${this.url}:${this.port}/api`;
+    }
+};
+
+const requestEndpoint = (method, route, desc, sleepSec = 1, body = null, params = {}) => {
+    const res = http.request(method, `${host.toString()}${route}`, body, params);
+    console.log(`${desc} status: ${res.status}`);
+    const success= check(res, { [`${desc} OK`]: (r) => r.status >= 200 && r.status < 300 });
+    if( !success) {
+      console.error(`${desc} failed with status ${res.status} and body: ${res.body}`);
+    }
+    sleep(sleepSec);
 };
 
 export default function () {
-  const email = `user_${randomString(5)}@test.com`;
-  const password = 'Test1234!';
-  const role = randomItem(['BUYER', 'ADMINISTRATOR', 'CONCESIONARY']);
+    sleep(10)
+    const emailreg = `user_${uuidv4()}@test.com`;
+    const passwordreg = 'Test1234!';
+    const role = randomItem(['BUYER', 'CONCESIONARY']);
+    const loginReq = {email: 'buyer@example.com', password: '12345678'};
+    const registerBody = {
+        fullname: 'Test User',
+        email:emailreg,
+        password:passwordreg,
+        role,
+    };
 
-  const registerBody = {
-    fullname: 'Test User',
-    email,
-    password,
-    role,
-  };
+    if (role === 'CONCESIONARY') {
+        registerBody.concesionaryCuit = `20${Math.floor(100000000 + Math.random() * 900000000)}`; 
+    }
 
-  if (role === 'CONCESIONARY') {
-    registerBody.concesionaryName = `Concesionaria_${randomString(4)}`;
-    registerBody.concesionaryCuit = `20${Math.floor(Math.random() * 100000000)}`;
-  }
+    const headers = { 'Content-Type': 'application/json' };
+    const loginPayload = JSON.stringify(loginReq);
 
-  const registerHeaders = { 'Content-Type': 'application/json' };
+    requestEndpoint(
+        'POST',
+        '/auth/register',
+        'User Registration',
+        1,
+        JSON.stringify(registerBody),
+        { headers }
+    );
 
-  const registerRes = http.post(
-    'http://localhost:4000/api/auth/register',
-    JSON.stringify(registerBody),
-    { headers: registerHeaders }
-  );
-
-  check(registerRes, { 'register status 201': (r) => r.status === 201 });
-
-  if (registerRes.status !== 201) {
-    console.log('❌ REGISTER ERROR:', registerRes.status, registerRes.body);
-    return; // no intento login si el registro falló
-  }
-
-  const loginPayload = JSON.stringify({ email, password });
-  const loginRes = http.post(
-    'http://localhost:4000/api/auth/login',
-    loginPayload,
-    { headers: registerHeaders }
-  );
-
-  check(loginRes, {
-    'login status 200': (r) => r.status === 200,
-    'has accessToken': (r) => {
-      try {
-        const data = JSON.parse(r.body);
-        return !!data.accessToken;
-      } catch {
-        return false;
-      }
-    },
-  });
-
-  if (loginRes.status !== 200) {
-    console.log('❌ LOGIN ERROR:', loginRes.status, loginRes.body);
-  }
-
-  sleep(1);
+    requestEndpoint(
+        'POST',
+        '/auth/login',
+        'User Login',
+        1,
+        loginPayload,
+        { headers }
+    );
 }
